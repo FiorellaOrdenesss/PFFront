b384276580d513836120bfeb16debab5bd59cac1
FiorellaOrdenesss <fiordenesvt@gmail.com>
2026-06-23 18:53:36 -0300

    traer cambios de fio, se agrego administrador y se modifico productos

--- src/components/NavbarProductos.jsx
+++ src/components/NavbarProductos.jsx
@@ -21,7 +21,7 @@ function NavbarProductos({ busqueda, setBusqueda, carrito, onCarritoClick }) {
           onClick={onCarritoClick}
         >
           Carrito
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          <span className="position-absolute top-0 s-100 translate-middle badge rounded-pill bg-danger">
             {carrito.length}
           </span>
         </button>
